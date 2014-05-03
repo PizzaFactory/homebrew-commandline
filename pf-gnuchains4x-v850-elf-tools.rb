ecae64f94ceeba816b4075019dfa2de70c360504
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-03 10:23:03 +0900

    Update source archive to pf3gnuchains4x-4.6.4-20140428.tgz.

--- pf-gnuchains4x-v850-elf-tools.rb
+++ pf-gnuchains4x-v850-elf-tools.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class PfGnuchains4xV850ElfTools < Formula
   homepage 'http://www.pizzafactory.jp/'
-  url 'https://bitbucket.org/pizzafactory/pf3gnuchains4x/downloads/pf3gnuchains4x-20140428.tgz'
+  url 'https://bitbucket.org/pizzafactory/pf3gnuchains4x/downloads/pf3gnuchains4x-4.6.4-20140428.tgz'
   sha1 '217c2e3f3bdb6729e1e75b1a6eb6a03a04b6bf69'
 
   head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
