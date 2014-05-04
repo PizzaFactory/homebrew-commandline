1efbe187da803137f5403b64b1a9e989d710cc89
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-04 19:30:44 +0900

    pf-gnuchains4x-mips64-elf-tools: add 4.6.4-20140428 bottle.

--- pf-gnuchains4x-mips64-elf-tools.rb
+++ pf-gnuchains4x-mips64-elf-tools.rb
@@ -7,6 +7,11 @@ class PfGnuchains4xMips64ElfTools < Formula
 
   head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
 
+  bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-tools-0.0"
+    sha1 "9b4ef078a2de6bea4e09a8c3c476e95937f9ac2f" => :mavericks
+  end
+
   depends_on :autoconf => :build
   depends_on :automake => :build
   depends_on :libtool => :build
