70c3769ba90f74f041e7fe26e203749ccccd6d61
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-04 19:30:44 +0900

    pf-gnuchains4x-xstormy16-elf-tools: add 4.6.4-20140428 bottle.

--- pf-gnuchains4x-xstormy16-elf-tools.rb
+++ pf-gnuchains4x-xstormy16-elf-tools.rb
@@ -7,6 +7,11 @@ class PfGnuchains4xXstormy16ElfTools < Formula
 
   head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
 
+  bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-tools-0.0"
+    sha1 "fdf3ab091397f17e5aefb650df27e33c5249da14" => :mavericks
+  end
+
   depends_on :autoconf => :build
   depends_on :automake => :build
   depends_on :libtool => :build
