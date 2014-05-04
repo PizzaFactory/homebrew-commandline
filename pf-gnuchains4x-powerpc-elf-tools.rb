f390356b5495a2954f6b8e9b0de19716242eeb97
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-04 19:30:44 +0900

    pf-gnuchains4x-powerpc-elf-tools: add 4.6.4-20140428 bottle.

--- pf-gnuchains4x-powerpc-elf-tools.rb
+++ pf-gnuchains4x-powerpc-elf-tools.rb
@@ -7,6 +7,11 @@ class PfGnuchains4xPowerpcElfTools < Formula
 
   head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
 
+  bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-tools-0.0"
+    sha1 "1f537746dec3279dd5989cdb5a10594c450cdd39" => :mavericks
+  end
+
   depends_on :autoconf => :build
   depends_on :automake => :build
   depends_on :libtool => :build
