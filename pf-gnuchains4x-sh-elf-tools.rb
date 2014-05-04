0120db3972160a9941971b88a204149269c519ea
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-04 19:30:44 +0900

    pf-gnuchains4x-sh-elf-tools: add 4.6.4-20140428 bottle.

--- pf-gnuchains4x-sh-elf-tools.rb
+++ pf-gnuchains4x-sh-elf-tools.rb
@@ -7,6 +7,11 @@ class PfGnuchains4xShElfTools < Formula
 
   head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
 
+  bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-tools-0.0"
+    sha1 "91090cecade5dddf281b02fa70d2c0faa5474b05" => :mavericks
+  end
+
   depends_on :autoconf => :build
   depends_on :automake => :build
   depends_on :libtool => :build
