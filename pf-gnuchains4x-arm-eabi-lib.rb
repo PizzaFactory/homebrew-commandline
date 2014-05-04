8ce90a3c5561e63ec59569b4636443435e549a8e
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-04 20:29:22 +0900

    Fix for arm-eabi.

--- pf-gnuchains4x-arm-eabi-lib.rb
+++ pf-gnuchains4x-arm-eabi-lib.rb
@@ -5,28 +5,58 @@ class PfGnuchains4xArmEabiLib < Formula
   url 'https://bitbucket.org/pizzafactory/pf3gnuchains4x/downloads/pf3gnuchains4x-20140428.tgz'
   sha1 '217c2e3f3bdb6729e1e75b1a6eb6a03a04b6bf69'
 
+  head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
+
+  depends_on :autoconf => :build
+  depends_on :automake => :build
+  depends_on :libtool  => :build
   depends_on "pf-gnuchains4x-arm-eabi-tools"
 
-  resource "tools" do
-    url "file:///usr/local/Library/Taps/pizzafactory/homebrew-commandline/pf-gnuchains4x-arm-eabi-tools-20140428.mavericks.bottle.tar.gz"
-    sha1 '80bf3204091b7146945cc97e29babb6f07cd0065'
-  end
-
-  resource "libs" do
-    url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/test-0.0/pf-gnuchains4x-arm-eabi-20140428.any.bottle.tar.gz"
-  end
-
   def install
-    resource("libs").stage do
-      cp_r "#{version}/arm-pizzafactory-eabi", prefix
+    ENV.j1
+
+    target='arm-pizzafactory-eabi'
+
+    system "sh 00pizza-generate-link.sh"
+
+    Dir.mkdir 'build'
+    cd 'build' do
+      system "../configure", "--quiet",
+                            "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}",
+                            "--target=#{target}",
+                            "--disable-gdbtk",
+                            "--disable-tui",
+                            "--disable-rda",
+                            "--enable-interwork",
+                            "--enable-multilib",
+                            "--with-newlib",
+                            "--without-headers",
+                            "--enable-languages=c,c++",
+                            "--with-bugurl=http://sourceforge.jp/projects/pf3gnuchains/ticket/",
+                            "--datarootdir=#{share}/#{target}",
+                            "--mandir=#{man}",
+                            "--disable-binutils",
+                            "--disable-ld",
+                            "--disable-gas",
+                            "--disable-gdb",
+                            "--disable-sim"
+
+      [ "gcc", "target-libstdc++-v3", "target-newlib", "target-libgloss" ].each do |t|
+        ohai "Building #{t}..."
+        %x[make all-#{t}]
+        ohai "Building #{t}...finished."
+      end
+      [ "target-libstdc++-v3", "target-newlib", "target-libgloss" ].each do |t|
+        ohai "Installing #{t}..."
+        %x[make install-#{t}]
+        ohai "Installing #{t}...finished."
+      end
     end
-    resource("tools").stage do
-      cp_r "#{version}/arm-pizzafactory-eabi", prefix
-      cp_r "#{version}/bin", prefix
-      cp_r "#{version}/lib", prefix
-      cp_r "#{version}/libexec", prefix
-      cp_r "#{version}/share", prefix
-    end
+    man7.rmtree
+    include.rmtree
   end
 
   test do
