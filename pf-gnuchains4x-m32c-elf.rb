ee9e45816a003ad5824de641b7eefaeb3bdad53e
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-10 20:46:32 +0900

    Support pf-gnuchains4x-m32c-elf.

---
+++ pf-gnuchains4x-m32c-elf.rb
@@ -0,0 +1,74 @@
+require "formula"
+
+class PfGnuchains4xM32cElf < Formula
+  homepage 'http://www.pizzafactory.jp/'
+  url 'https://bitbucket.org/pizzafactory/pf3gnuchains4x/downloads/pf3gnuchains4x-4.6.4-20140428.tgz'
+  sha1 '217c2e3f3bdb6729e1e75b1a6eb6a03a04b6bf69'
+
+  head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
+
+  bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/1.0.0-beta1"
+    sha1 "8931fafce67caec00895ad3ede167e38e5c11b56" => :mavericks
+  end
+
+  depends_on :autoconf => :build
+  depends_on :automake => :build
+  depends_on :libtool => :build
+
+  def install
+    ENV.j1
+
+    system "sh 00pizza-generate-link.sh"
+
+    target='m32c-pizzafactory-elf'
+
+    Dir.mkdir 'build'
+    cd 'build' do
+      system "../configure", "--quiet",
+                            "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}",
+                            "--target=#{target}",
+                            "--disable-gdbtk",
+                            "--disable-tui",
+                            "--disable-rda",
+                            "--enable-interwork",
+                            "--enable-multilib",
+                            "--with-newlib",
+                            "--without-headers",
+                            "--enable-languages=c,c++",
+                            "--with-bugurl=http://sourceforge.jp/projects/pf3gnuchains/ticket/",
+                            "--datarootdir=#{share}/#{target}",
+                            "--mandir=#{man}"
+      [ "binutils", "ld", "gas", "gdb", "sim", "gcc", "target-libgcc" ].each do |t|
+        ohai "Building #{t}..."
+        %x[make all-#{t}]
+        ohai "Building #{t}...finished."
+      end
+      [ "binutils", "ld", "gas", "gdb", "sim", "gcc", "target-libgcc" ].each do |t|
+        ohai "Installing #{t}..."
+        %x[make install-#{t}]
+        ohai "Installing #{t}...finished."
+      end
+    end
+    man7.rmtree
+    include.rmtree
+    resource("libs").stage do
+      cd Dir['*'][0] do
+        cp_r 'share', prefix
+        cp_r Dir['*-*-*'], prefix
+      end
+    end
+  end
+
+  resource 'libs' do
+    url 'https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-libs-0.0/pf-gnuchains4x-m32c-elf-lib-20140428.mavericks.bottle.tar.gz'
+    sha1 'c3c3dd73c6833a2c86436dc54e78fa96b19fa19d' #sha1-lib-
+  end
+
+  test do
+    system "m32c-pizzafactory-elf-gcc", "--help"
+  end
+end
