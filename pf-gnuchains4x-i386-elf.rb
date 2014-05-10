e2cda8c7a0db97001d2a1e91fcd0372d11429aaf
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-10 15:37:10 +0900

    pf-gnuchains4x-i386-elf: update 4.6.4-20140428 bottle.

--- pf-gnuchains4x-i386-elf.rb
+++ pf-gnuchains4x-i386-elf.rb
@@ -8,7 +8,7 @@ class PfGnuchains4xI386Elf < Formula
   head 'http://bitbucket.org/pizzafactory/pf3gnuchains4x.git'
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/1.0.0-beta1"
+    sha1 "4b2b9510f8fe6c86be80ba61ed0282097387c697" => :mavericks
   end
 
   depends_on :autoconf => :build
