140595a6c566cffa3871da611913da78138d65f8
Travis CI <noreply@travis-ci.com>
2014-05-10 20:40:58 -0700

    urjtag: update 0.10 bottle.

--- urjtag.rb
+++ urjtag.rb
@@ -6,9 +6,9 @@ class Urjtag < Formula
   sha1 "f44e666ae484f5a7e3b50574db84df646a8d9fdb"
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/1.0.0-beta1"
-    revision 1
-    sha1 "a246341346276a69e97328c155c1e440df17241d" => :mavericks
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-2"
+    revision 2
+    sha1 "29f210df007f0cb286b4cebb207f28a4ec0cd671" => :mavericks
   end
 
   depends_on "libusb"
