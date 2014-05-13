ac814d795b1fc1be25f1ae56248ba0984015fb80
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-13 11:52:38 +0900

    Fix for nios2-elf.

---
+++ pf-gnuchains4x-nios2-elf.rb
@@ -0,0 +1,68 @@
+require "formula"
+
+class PfGnuchains4xNios2Elf < Formula
+  homepage 'http://www.pizzafactory.jp/'
+  url 'https://bitbucket.org/pizzafactory/pf-binutils-gdb/downloads/pf-binutils-gdb-4.6.4-20140512.tar.gz'
+  sha1 '55efd85ba1a78c98fb65ce309ce454c8e38aec09'
+
+  head 'https://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
+
+  bottle do
+  end
+
+  depends_on :autoconf => :build
+  depends_on :automake => :build
+  depends_on :libtool => :build
+
+  def install
+    ENV.j1
+
+    target='nios2-pizzafactory-elf'
+
+    system "sh 00pizza-generate-link.sh"
+
+    Dir.mkdir 'build'
+    cd 'build' do
+      system "../configure", "--quiet", "--disable-werror",
+                            "--disable-debug",
+                            "--disable-dependency-tracking",
+                            "--disable-silent-rules",
+                            "--prefix=#{prefix}",
+                            "--target=#{target}",
+                            "--disable-gdbtk",
+                            "--disable-tui",
+                            "--disable-rda",
+                            "--enable-interwork",
+                            "--enable-multilib",
+                            "--with-newlib",
+                            "--without-headers",
+                            "--enable-languages=c,c++",
+                            "--with-bugurl=http://sourceforge.jp/projects/pf3gnuchains/ticket/",
+                            "--datarootdir=#{share}/#{target}",
+                            "--mandir=#{man}"
+      [ "binutils", "ld", "gas", "gdb", "sim", "gcc", "target-libgcc" ].each do |t|
+        system 'make', "all-#{t}"
+      end
+      [ "binutils", "ld", "gas", "gdb", "sim", "gcc", "target-libgcc" ].each do |t|
+        system 'make', "install-#{t}"
+      end
+    end
+    man7.rmtree
+    include.rmtree
+    resource("libs").stage do
+      cd Dir['*'][0] do
+        cp_r 'share', prefix
+        cp_r Dir['*-*-*'], prefix
+      end
+    end
+  end
+
+  resource 'libs' do
+    url 'https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-libs-0.0/pf-gnuchains4x-nios2-elf-lib-20140428.mavericks.bottle.tar.gz'
+    sha1 '56c9b86e9b9d5dd1bee62f4f38ff5ff3c4697f7b' #sha1-lib-
+  end
+
+  test do
+    system "nios2-pizzafactory-elf-gcc", "--help"
+  end
+end
