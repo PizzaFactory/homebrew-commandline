e13f158221ad80aeb794fdee296f7c042cec9b27
Travis CI <noreply@travis-ci.com>
2014-05-14 10:01:02 -0700

    pf-gnuchains4x-m32r-elf-tools: update 4.6.4-20140514 bottle.

--- pf-gnuchains4x-m32r-elf-tools.rb
+++ pf-gnuchains4x-m32r-elf-tools.rb
@@ -8,8 +8,8 @@ class PfGnuchains4xM32rElfTools < Formula
   head 'http://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-tools-0.0"
-    sha1 "329b1b347a88d42b3f4df7845246121b10dc3841" => :mavericks
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-gnuchains-tools-0.7"
+    sha1 "2119a5bb55ab7f097fc8b0d7077d552892881ee6" => :mavericks
   end
 
   depends_on :autoconf => :build
