c8deabcac5f541d6a23927e58e651e4377f5c501
Masaki Muranaka <monaka@monami-ya.jp>
2014-05-16 15:38:44 +0900

    Support bfin-gdbproxy. Fixes #11.

---
+++ bfin-gdbproxy.rb
@@ -0,0 +1,31 @@
+require "formula"
+
+class BfinGdbproxy < Formula
+  homepage "http://urjtag.org/"
+  url 'https://github.com/pf3gnuchains/bfin-gdbproxy/archive/pf-gdbproxy-0.7.2.20140516.tar.gz'
+  sha1 "bc5aa99c7c851326b8ccb45a9bbe7bc5ab7d4502"
+
+  bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-2"
+    revision 2
+    sha1 "29f210df007f0cb286b4cebb207f28a4ec0cd671" => :mavericks
+  end
+
+  depends_on "urjtag"
+  depends_on "pkg-config" => :build
+
+  def install
+
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--program-prefix=bfin-"
+
+    system "make", "install"
+  end
+
+  test do
+    system "bfin-gdbproxy", "--version"
+  end
+end
