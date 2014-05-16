91f7f3e715001a3908e4e3e07964449c54807002
Travis CI <noreply@travis-ci.com>
2014-05-15 22:52:55 -0700

    pf-gnuchains4x-mips64-elf-lib: update 4.6.4-20140516 bottle.

--- pf-gnuchains4x-mips64-elf-lib.rb
+++ pf-gnuchains4x-mips64-elf-lib.rb
@@ -8,6 +8,8 @@ class PfGnuchains4xMips64ElfLib < Formula
   head 'http://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
   bottle do
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-gnuchains-libs-0.5"
+    sha1 "a04426d798be255d5d66acfc2268664cc501ecf0" => :mavericks
   end
 
   depends_on :autoconf => :build
