4a077a3c68667ae1b789c0eebf8139f0ea1e1a06
Travis CI <noreply@travis-ci.com>
2014-05-16 01:32:47 -0700

    pf-gnuchains4x-arm-eabi: update 4.6.4-20140514 bottle.

--- pf-gnuchains4x-arm-eabi.rb
+++ pf-gnuchains4x-arm-eabi.rb
@@ -8,8 +8,8 @@ class PfGnuchains4xArmEabi < Formula
   head 'https://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-6"
-    sha1 "79247ec1d739976b7ce3daf0be3d04cb8d740869" => :mavericks
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-hostfix-7"
+    sha1 "b2a65251e8c0ba9ff4a37fbd10d1ee3b7ea024a2" => :mavericks
   end
 
   depends_on :autoconf => :build
