ad6a110ee632a4de3185e7e8c1d31ee4da151cbc
Travis CI <noreply@travis-ci.com>
2014-05-16 01:46:37 -0700

    pf-gnuchains4x-bfin-elf: update 4.6.4-20140514 bottle.

--- pf-gnuchains4x-bfin-elf.rb
+++ pf-gnuchains4x-bfin-elf.rb
@@ -8,8 +8,8 @@ class PfGnuchains4xBfinElf < Formula
   head 'https://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-6"
-    sha1 "e6fab87ec575ef845d7f04605a80485ff498e41f" => :mavericks
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-hostfix-7"
+    sha1 "6304ac6a8908faaad8bdca25d9a5bf57772d6b6d" => :mavericks
   end
 
   depends_on :autoconf => :build
