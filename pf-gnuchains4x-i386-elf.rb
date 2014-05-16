cfb2ebfe19da1e63bcf32f7f9c5d4ed08772efda
Travis CI <noreply@travis-ci.com>
2014-05-16 02:14:30 -0700

    pf-gnuchains4x-i386-elf: update 4.6.4-20140514 bottle.

--- pf-gnuchains4x-i386-elf.rb
+++ pf-gnuchains4x-i386-elf.rb
@@ -8,8 +8,8 @@ class PfGnuchains4xI386Elf < Formula
   head 'https://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/1.0.0-beta1"
-    sha1 "4b2b9510f8fe6c86be80ba61ed0282097387c697" => :mavericks
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-1.0.0-beta2-hostfix-7"
+    sha1 "996ffd4e3020114e4662dda36d8ace43373d5366" => :mavericks
   end
 
   depends_on :autoconf => :build
