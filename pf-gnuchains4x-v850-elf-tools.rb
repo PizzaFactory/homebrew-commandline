b5c03fdd4500edf04a61be194390a129a1e65130
Travis CI <noreply@travis-ci.com>
2014-05-18 06:16:46 -0700

    pf-gnuchains4x-v850-elf-tools: update 4.6.4-20140516 bottle.

--- pf-gnuchains4x-v850-elf-tools.rb
+++ pf-gnuchains4x-v850-elf-tools.rb
@@ -14,8 +14,8 @@ class PfGnuchains4xV850ElfTools < Formula
   head 'http://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
   bottle do
-    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-tools-0.0"
-    sha1 "771c0bd3f5ff8271f665f51e8de955f8fdb9307c" => :mavericks
+    root_url "https://github.com/PizzaFactory/homebrew-commandline/releases/download/release-gnuchains-tools-0.10"
+    sha1 "bf3dffd258d97993caeb4c58f075c6cc1c6b7cd8" => :mavericks
   end
 
   depends_on :autoconf => :build
