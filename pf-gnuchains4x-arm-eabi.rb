ecf89d9e6fa37cfa0c2e8a35b04256165ebe18e3
Masaki Muranaka <monaka@monami-ya.jp>
2014-06-03 11:12:23 +0900

    Update the source tree.

--- pf-gnuchains4x-arm-eabi.rb
+++ pf-gnuchains4x-arm-eabi.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class PfGnuchains4xArmEabi < Formula
   homepage 'http://www.pizzafactory.jp/'
-  url 'https://bitbucket.org/pizzafactory/pf-binutils-gdb/downloads/pf-binutils-gdb-4.6.4-20140516.tar.gz'
-  sha1 '4b14822c6afeb6c554428dec3dfc58a0f40a9dbe'
+  url 'https://bitbucket.org/pizzafactory/pf-binutils-gdb/downloads/pf-binutils-gdb-4.7.4-20140603.tar.gz'
+  sha1 'bd9e984efd0ad018a1f63df4fdfd98bbc7294e0e'
 
   head 'https://bitbucket.org/pizzafactory/pf-binutils-gdb.git'
 
@@ -63,8 +63,8 @@ class PfGnuchains4xArmEabi < Formula
   end
 
   resource 'libs' do
-    url 'https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-libs-0.0/pf-gnuchains4x-arm-eabi-lib-20140428.mavericks.bottle.tar.gz'
-    sha1 '5ffedfacdd20816a117bd06ac5c7795ac0779f01' #sha1-lib-
+    url 'https://github.com/PizzaFactory/homebrew-commandline/releases/download/gnuchains-libs-0.7/pf-gnuchains4x-arm-eabi-lib-4.6.4-20140516.mavericks.bottle.tar.gz'
+    sha1 '6063e22cbb7bf0157e27eb153287836561174cbe' #sha1-lib-
   end
 
   test do
